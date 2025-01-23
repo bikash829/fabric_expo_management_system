22401da97095340a646a4ab1a2a0e9a762a30bf1
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-01-23 16:41:15 +0600

    1.1 Roles and Permissions (4.Add password change functionality and update user profile template)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path,include
+from django.urls import path,include, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path(
+        "password_change/", auth_views.PasswordChangeView.as_view(
+            success_url=reverse_lazy("accounts:password_change_done")
+        ), name="password_change"
+    ),
     path("", include("django.contrib.auth.urls")),
 
     # user info

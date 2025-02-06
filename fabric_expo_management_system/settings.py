9e80811508ad1699dc7bd07621165fe913c5d992
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-02-06 13:01:07 +0600

    Deploy (10. remove sensitive information from setting file   )

--- fabric_expo_management_system/settings.py
+++ fabric_expo_management_system/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-8ksu95hb05$z1i@dszn@&kpe4l%j22n&ws*^o4(+8v*un8p4cr"
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -140,7 +139,6 @@ STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # media directory
-MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 

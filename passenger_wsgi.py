410d0e17b0bd5e725dcdcb46d700923d91adc419
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-06-23 17:12:31 +0600

    updated wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,6 +2,6 @@ import sys
 import os
 
 # Add project directory to sys.path
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+# sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from fabric_expo_management_system.wsgi import application

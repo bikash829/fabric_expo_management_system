22b9bbd0d03a5352378cb768147cd50ee2bbfe43
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-06-24 02:37:10 +0600

    fix: ensure OPENBLAS_NUM_THREADS is set in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,4 +4,6 @@ import os
 # Add project directory to sys.path
 # sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
 from fabric_expo_management_system.wsgi import application

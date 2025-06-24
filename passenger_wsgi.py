34bd60bc7b0cb9472ebd879a3f30b19d612ac6fc
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-06-24 11:30:17 +0600

    update gitignore

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,9 @@
-import sys
-import os
+# import sys
+# import os
 
-# Add project directory to sys.path
-# sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+# # Add project directory to sys.path
+# # sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
+# os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
-from fabric_expo_management_system.wsgi import application
+# from fabric_expo_management_system.wsgi import application

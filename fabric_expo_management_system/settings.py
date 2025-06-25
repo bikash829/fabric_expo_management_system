e9d69b1bc7b2f419b99e9e41bed06519dd9a5d70
Bikash Chowdhury <chowdhurybikash747@gmail.com>
2025-06-25 12:45:15 +0600

    update setting static file dir

--- fabric_expo_management_system/settings.py
+++ fabric_expo_management_system/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # media directory

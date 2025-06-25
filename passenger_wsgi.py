d0ba4f6d47e982be70e4f02f951a4edbab3192f9
Bikash Chowdhury <chowdhurybikash747@gmail.com>
2025-06-25 14:15:52 +0600

    update passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,6 @@
-# import sys
-# import os
+import os
+import sys
 
-# # Add project directory to sys.path
-# # sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
-# os.environ["OPENBLAS_NUM_THREADS"] = "1"
-
-# from fabric_expo_management_system.wsgi import application
+from fabric_expo_management_system.wsgi import application

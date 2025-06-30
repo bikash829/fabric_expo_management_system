74ef935aa0440d1b8001901189f413f2c057b1f4
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2025-06-30 14:36:59 +0600

    info.py file is updated

--- fabric_expo_management_system/info.py
+++ fabric_expo_management_system/info.py
@@ -5,9 +5,10 @@ from business_data.models import CompanyProfile
 business_infos =  CompanyProfile.objects.all() 
 
 business_info = business_infos.first() if business_infos.exists() else None
-PROJECT_NAME = "ExpoSync"
 
-# PROJECT_NAME = business_info.get_company_name_display() if business_info else "ExpoSync"
+
+PROJECT_NAME = business_info.get_company_name_display() if business_info else "ExpoSync"
+COMPANY_LOGO = "/static/images/logo.png"
 COMPANY_LOGO = business_info.logo.url if business_info and business_info.logo else "/static/images/logo.png"
 ADDRESS = business_info.address if business_info and business_info.address else ""
 PHONE_NUMBER = str(business_info.phone_number) if business_info and business_info.phone_number else ""

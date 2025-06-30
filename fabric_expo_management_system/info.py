626cd5cf6e44e227904acbc2c64e98c75dbe385d
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2025-06-30 15:53:44 +0600

    company logo has been updated

--- fabric_expo_management_system/info.py
+++ fabric_expo_management_system/info.py
@@ -8,7 +8,7 @@ business_info = business_infos.first() if business_infos.exists() else None
 
 
 PROJECT_NAME = business_info.get_company_name_display() if business_info else "ExpoSync"
-COMPANY_LOGO = business_info.logo.url if business_info and business_info.logo else "/static/images/logo.png"
+COMPANY_LOGO = business_info.logo.url if business_info and business_info.logo else "/media/images/logo/fabric_expo.jpeg"
 ADDRESS = business_info.address if business_info and business_info.address else ""
 PHONE_NUMBER = str(business_info.phone_number) if business_info and business_info.phone_number else ""
 EMAIL = business_info.email if business_info and business_info.email else ""

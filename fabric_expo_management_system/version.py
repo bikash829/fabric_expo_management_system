b2abf7bad0e3e318e47d7fc73c588507defc7808
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2026-02-22 13:19:53 +0600

    version control

--- fabric_expo_management_system/version.py
+++ fabric_expo_management_system/version.py
@@ -1,13 +1,18 @@
 import subprocess
+import re
 
 def get_git_version():
     try:
         version = subprocess.check_output(
             ["git", "describe", "--tags", "--always"],
+            # ["git", "tag"],
             stderr=subprocess.DEVNULL
         ).decode().strip()
+        # Keep only the main tag, drop -3-gabc1234
+        version = re.split(r"-", version)[0]
+        
         return version
     except Exception:
-        return "unknown"
+        return "v0.0.0"
 
 __version__ = get_git_version()
